71fc7729cfefff45bc70c10229c16e22128af6de
louis <jaume.l@etoncollege.org.uk>
2023-02-04 10:20:30 +0000

    ar4

--- AbsenceRecorder/Model/Division.swift
+++ AbsenceRecorder/Model/Division.swift
@@ -17,7 +17,21 @@ class Division {
     }
     
     func getAbsence(for date: Date) -> Absence? {
-        return absences.first
+        return absences.first {
+            let comparison = Calendar.current.compare($0.takenOn, to: date, toGranularity: .day)
+            return comparison == .orderedSame
+        }
+    }
+    
+    func createAbsenceOfGetExistingIfAvailable(for date: Date) -> Absence {
+        if let existingAbsence = getAbsence(for: date) {
+            return existingAbsence
+        } else {
+            let absence = Absence(date: date, students: students)
+            absences.append(absence)
+            
+            return absence
+        }
     }
     
     #if DEBUG
--- AbsenceRecorder/View/DivisionsView.swift
+++ AbsenceRecorder/View/DivisionsView.swift
@@ -14,7 +14,7 @@ struct DivisionsView: View {
     var body: some View {
         NavigationView {
             List(state.divisions, id: \.self.code) { division in
-                NavigationLink(destination: AbsenceView(absence: Absence(date: Date(), students: division.students))) {
+                NavigationLink(destination: AbsenceView(absence: division.createAbsenceOfGetExistingIfAvailable(for: currentDate))) {
                     DivisionItem(division: division)
                 }
                 
